ff415e36c41b116d6465c3b510de64e070eeabf5
lingulist <mattis.list@lingulist.de>
2018-09-23 14:11:31 +0200

    update sinopy

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 #distribute_setup.use_setuptools()
 
 from setuptools import setup, find_packages,Extension
+import codecs
 # setup package name etc as a default
 pkgname = 'sinopy'
 
 
 setup(
         name=pkgname,
-        version='0.3.0',
+        description="A Python library for quantitative tasks in Chinese historical linguistics.",
+        version='0.3.1',
         packages=find_packages(where='src'),
         package_dir={'': 'src'},
         zip_safe=False,
@@ -16,7 +18,7 @@ setup(
         include_package_data=True,
         install_requires=['lingpy'],
         url='https://github.com/lingpy/sinopy',
-        long_description=open('README.md').read(),
+        long_description=codecs.open('README.md', 'r', 'utf-8').read(),
         long_description_content_type='text/markdown',
         entry_points={
             'console_scripts': ['sinopy=sinopy.cli:main'],
